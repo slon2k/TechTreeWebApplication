7263a03193e1a10a6cca9da5ec21b10cc251b432
Boris Korobeinikov <slon2k@gmail.com>
2022-08-31 17:21:14 +0600

    Added Migration ContentCreated

---
+++ TechTreeWebApplication/Data/Migrations/20220831112017_ContentCreated.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechTreeWebApplication.Data.Migrations
+{
+    public partial class ContentCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Content",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryItemId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Html = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: true),
+                    VideoLink = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Content", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Content_CategoryItem_CategoryItemId",
+                        column: x => x.CategoryItemId,
+                        principalTable: "CategoryItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Content_CategoryItemId",
+                table: "Content",
+                column: "CategoryItemId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Content");
+        }
+    }
+}
