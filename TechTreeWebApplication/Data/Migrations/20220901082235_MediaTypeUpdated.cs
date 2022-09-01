db19e4004faf5332e2e498b73a204bd78da31d12
Boris Korobeinikov <slon2k@gmail.com>
2022-09-01 14:23:19 +0600

    Migration MediaType add data

---
+++ TechTreeWebApplication/Data/Migrations/20220901082235_MediaTypeUpdated.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechTreeWebApplication.Data.Migrations
+{
+    public partial class MediaTypeUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MediaType",
+                columns: new[] { "Id", "Thumbnail", "Title" },
+                values: new object[] { 1, "/images/ArticleImage.jpeg", "Article" });
+
+            migrationBuilder.InsertData(
+                table: "MediaType",
+                columns: new[] { "Id", "Thumbnail", "Title" },
+                values: new object[] { 2, "/images/VideoImage.jpeg", "Video" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MediaType",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MediaType",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
