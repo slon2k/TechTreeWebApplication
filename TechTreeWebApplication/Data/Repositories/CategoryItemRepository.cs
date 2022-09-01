58f10c719d09b88c1828c3ed41d90523b275aa03
Boris Korobeinikov <slon2k@gmail.com>
2022-09-01 14:20:58 +0600

    created Category Item Repository

---
+++ TechTreeWebApplication/Data/Repositories/CategoryItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using TechTreeWebApplication.Entities;
+using TechTreeWebApplication.Interfaces;
+
+namespace TechTreeWebApplication.Data.Repositories
+{
+    public class CategoryItemRepository : RepositoryBase<CategoryItemEntity, int>, ICategoryItemRepository
+    {
+        public CategoryItemRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ TechTreeWebApplication/Interfaces/ICategoryItemRepository.cs
@@ -0,0 +1,8 @@
+﻿using TechTreeWebApplication.Entities;
+
+namespace TechTreeWebApplication.Interfaces
+{
+    public interface ICategoryItemRepository : IRepository<CategoryItemEntity, int>
+    {
+    }
+}
