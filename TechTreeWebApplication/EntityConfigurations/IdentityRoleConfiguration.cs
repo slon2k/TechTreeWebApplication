3263c6f33be53ae9f4ade06e7dd1f96c50474acd
Boris Korobeinikov <slon2k@gmail.com>
2022-09-30 14:56:11 +0600

    added roles seeding

---
+++ TechTreeWebApplication/EntityConfigurations/IdentityRoleConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TechTreeWebApplication.EntityConfigurations
+{
+    public class IdentityRoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        private const string AdminName = "Admin";
+        private const string UserName = "User";
+
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(new List<IdentityRole>
+            {
+                new IdentityRole{ Name=AdminName, NormalizedName=AdminName.ToUpper()},
+                new IdentityRole{ Name=UserName, NormalizedName=UserName.ToUpper()}
+            });
+        }
+    }
+}
